dd97df3b886300eae45b0e7f3ff8718a4affc91f
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-12-03 04:57:15 -0800

    Improve ping script

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-echo "127.0.0.1\n"
+function query_eth_blockNumber() {
+    local host=$1
+    echo
+    echo "Querying: $host"
+    echo
 
-curl http://127.0.0.1:8545 --request POST \
-  --header "Content-Type: application/json" \
-  --connect-timeout 3 \
-  --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+    curl http://$host:8545 --request POST \
+      --header "Content-Type: application/json" \
+      --connect-timeout 3 \
+      --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+}
 
-echo "localhost\n"
+# List of hosts to query
+hosts=("127.0.0.1" "localhost" "0.0.0.0" "host.docker.internal")
 
-curl http://localhost:8545 --request POST \
-  --header "Content-Type: application/json" \
-  --connect-timeout 3 \
-  --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
-
-echo -e "0.0.0.0\n"
-
-curl http://0.0.0.0:8545 --request POST \
-  --header "Content-Type: application/json" \
-  --connect-timeout 3 \
-  --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+# Loop through and query each host
+for host in "${hosts[@]}"; do
+    query_eth_blockNumber $host
+done
