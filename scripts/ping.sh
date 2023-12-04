e3de04cd11df02b3f82fa305e244348af5429b57
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-12-03 17:16:05 -0800

    Improve ping script

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -3,19 +3,37 @@
 function query_eth_blockNumber() {
     local host=$1
     echo
-    echo "Querying: $host"
     echo
+    echo "========================================"
+    echo "Querying: $host"
+    echo "========================================"
 
-    curl http://$host:8545 --request POST \
+
+    (set -x; curl http://$host:8545 --request POST \
       --header "Content-Type: application/json" \
       --connect-timeout 3 \
-      --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}'
+      --silent \
+      --show-error \
+      --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}';
+      if [ $? -eq 0 ]; then
+        # green success message
+        echo -e "\033[0;32mhttp://$host:8545 connection succeeded\033[0m"
+      else
+        # red error message
+        echo -e "\033[0;31mhttp://$host:8545 Failed to connect\033[0m"
+      fi
+      )
+    echo
 }
 
+echo "Ping starting..."
+
 # List of hosts to query
-hosts=("127.0.0.1" "localhost" "0.0.0.0" "host.docker.internal")
+hosts=("localhost" "0.0.0.0" "127.0.0.1" "host.docker.internal")
 
 # Loop through and query each host
 for host in "${hosts[@]}"; do
     query_eth_blockNumber $host
 done
+
+echo "Ping finished."
