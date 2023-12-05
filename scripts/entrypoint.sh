4376310ef28722823a94c16aed6b42c92fbd0a2a
Omar <github@omaraziz.dev>
2023-12-05 01:44:54 -0800

    nice logging

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "Starting indexer container...:"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo
-echo "Starting indexer container..."
-echo
-
-echo "Current database migrations status:"
+echo "========================================"
+Current "database migrations status:"
 bunx dbmate status
-echo
-
-echo "Setting up database and generating types..."
-bunx dbmate up
-echo
-
-echo "Current database migrations status:"
-bunx dbmate status
-echo
+echo "========================================"
 
 exec "$@"
