eafb18de270d652b6d6a6266a6a40baf923f93f1
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-12-24 23:56:35 -0800

    get_following, top following db funcs

--- db/migrations/006_Function_Definitions_Depends_On_Tables_And_Views.sql
+++ db/migrations/006_Function_Definitions_Depends_On_Tables_And_Views.sql
@@ -10,7 +10,7 @@
 -- Returns: A table with 'token_id' (bigint) and 'list_user' (varchar(255)),
 --          representing the relationship identifier and the follower's name.
 -------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION public.get_followers(address text)
+CREATE OR REPLACE FUNCTION public.get_followers(address character varying(42))
 RETURNS TABLE(token_id bigint, list_user character varying(255))
 LANGUAGE plpgsql
 AS $$
@@ -28,6 +28,8 @@ BEGIN
         lrtev.version = 1 AND
         -- only type 1 ("address record")
         lrtev.type = 1 AND
+        -- valid address format
+        lrtev.data ~ '^0x[a-f0-9]{40}$' AND
         -- NOT blocked
         lrtev.has_block_tag = FALSE AND
         -- NOT muted
@@ -51,7 +53,7 @@ $$;
 -- Returns: A table with 'list_user' (varchar(255)), representing unique names
 --          or identifiers of the followers.
 -------------------------------------------------------------------------------
-CREATE OR REPLACE FUNCTION public.get_unique_followers(address text)
+CREATE OR REPLACE FUNCTION public.get_unique_followers(address character varying(42))
 RETURNS TABLE(list_user character varying(255))
 LANGUAGE plpgsql
 AS $$
@@ -62,10 +64,17 @@ BEGIN
     FROM
         list_record_tags_extended_view AS lrtev
     WHERE
+        -- only version 1
         lrtev.version = 1 AND
+        -- address record type
         lrtev.type = 1 AND
+        -- valid address format
+        lrtev.data ~ '^0x[a-f0-9]{40}$' AND
+        -- NOT blocked
         lrtev.has_block_tag = FALSE AND
+        -- NOT muted
         lrtev.has_mute_tag = FALSE AND
+        -- match the address parameter
         lrtev.data = address
     ORDER BY
         lrtev.list_user ASC;
@@ -99,15 +108,113 @@ BEGIN
     FROM
         list_record_tags_extended_view AS lrtev
     WHERE
+        -- only version 1
         lrtev.version = 1 AND
+        -- address record type
         lrtev.type = 1 AND
+        -- valid address format
+        lrtev.data ~ '^0x[a-f0-9]{40}$' AND
+        -- NOT blocked
         lrtev.has_block_tag = FALSE AND
+        -- NOT muted
         lrtev.has_mute_tag = FALSE
     GROUP BY
         lrtev.data
     ORDER BY
         followers_count DESC,
         lrtev.data ASC
+    LIMIT limit_count;
+END;
+$$;
+
+
+
+-------------------------------------------------------------------------------
+-- Function: get_following
+-- Description: Retrieves a list of addresses that a specified user is following
+--              from the list_record_tags_extended_view, ensuring the addresses
+--              are valid 20-byte, lower-case hexadecimal (0x followed by 40
+--              hexadecimal characters). Filters tokens by version and type,
+--              excluding blocked or muted relationships.
+-- Parameters:
+--   - user_id (text): Identifier of the user to find the following addresses.
+-- Returns: A table with 'followed_address' (varchar(255)) and 'token_id' (bigint),
+--          representing valid addresses being followed and the relationship
+--          identifier.
+-------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION public.get_following(address character varying(42))
+RETURNS TABLE(token_id bigint, followed_address character varying(255))
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        -- the token id that corresponds to the following action
+        lrtev.token_id,
+        -- the address being followed
+        lrtev.data AS followed_address
+    FROM
+        list_record_tags_extended_view AS lrtev
+    WHERE
+        -- only version 1
+        lrtev.version = 1 AND
+        -- address record type
+        lrtev.type = 1 AND
+        -- NOT blocked
+        lrtev.has_block_tag = FALSE AND
+        -- NOT muted
+        lrtev.has_mute_tag = FALSE AND
+        -- user who is following
+        lrtev.list_user = address AND
+        -- valid address format
+        lrtev.data ~ '^0x[a-f0-9]{40}$'
+    ORDER BY
+        lrtev.token_id ASC,
+        lrtev.data ASC;
+END;
+$$;
+
+
+
+-------------------------------------------------------------------------------
+-- Function: count_unique_following_by_address
+-- Description: Counts the unique addresses that each user is following in the
+--              list_record_tags_extended_view, groups the results by user, and
+--              orders them by the number of unique addresses in descending
+--              order. Includes a LIMIT parameter to control the number of
+--              returned rows. The function filters by version and type,
+--              excluding blocked or muted relationships.
+-- Parameters:
+--   - limit_count (bigint): The maximum number of rows to return.
+-- Returns: tbd
+-------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION public.count_unique_following_by_address(limit_count bigint)
+RETURNS TABLE(address character varying(255), following_count bigint)
+LANGUAGE PLPGSQL
+AS $$
+BEGIN
+    RETURN QUERY
+  SELECT
+        lrtev.list_user AS address,
+        COUNT(DISTINCT lrtev.data) AS following_count
+    FROM
+        list_record_tags_extended_view AS lrtev
+    WHERE
+        -- only version 1
+        lrtev.version = 1 AND
+        -- address record type
+        lrtev.type = 1 AND
+        -- NOT blocked
+        lrtev.has_block_tag = FALSE AND
+        -- NOT muted
+        lrtev.has_mute_tag = FALSE AND
+        -- valid address format
+        lrtev.data ~ '^0x[a-f0-9]{40}$'
+    GROUP BY
+        lrtev.list_user
+    ORDER BY
+        following_count DESC,
+        lrtev.list_user ASC
     LIMIT limit_count;
 END;
 $$;
