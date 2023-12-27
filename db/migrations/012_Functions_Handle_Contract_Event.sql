3ac3c33e283c319be07b943f823b2068c7b3218d
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-12-27 01:48:09 -0800

    db func for ListStorageLocationChange

--- db/migrations/012_Functions_Handle_Contract_Event.sql
+++ db/migrations/012_Functions_Handle_Contract_Event.sql
@@ -3,6 +3,57 @@
 
 
 -------------------------------------------------------------------------------
+-- Function: handle_contract_event__ListStorageLocationChange
+-- Description: Processes a ListStorageLocationChange event by decoding the
+--              list storage location and updating the corresponding fields in
+--              the list_nfts table.
+-- Parameters:
+--   - p_chain_id (BIGINT): The blockchain network identifier.
+--   - p_contract_address (VARCHAR(42)): The contract address of the NFT.
+--   - p_token_id (BIGINT): The unique identifier of the NFT.
+--   - p_list_storage_location (VARCHAR(174)): The list storage location to be
+--                                             decoded and updated.
+-- Returns: VOID
+-- Notes: Uses the list_nfts and decode_list_storage_location functions for
+--        storage and decoding respectively.
+-------------------------------------------------------------------------------
+CREATE OR REPLACE FUNCTION public.handle_contract_event__ListStorageLocationChange(
+    p_chain_id BIGINT,
+    p_contract_address VARCHAR(42),
+    p_token_id BIGINT,
+    p_list_storage_location VARCHAR(174)
+)
+RETURNS VOID
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    normalized_contract_address types.eth_address;
+    -- {chain_id, contract_address, nonce}
+    decoded_location RECORD;
+BEGIN
+    -- Normalize the input addresses to lowercase
+    normalized_contract_address := public.normalize_eth_address(p_contract_address);
+
+    -- Decode the list storage location
+    decoded_location := public.decode_list_storage_location(p_list_storage_location);
+
+    -- Update list_nfts with the decoded values
+    UPDATE public.list_nfts
+    SET
+        list_storage_location = p_list_storage_location,
+        list_storage_location_chain_id = decoded_location.chain_id,
+        list_storage_location_contract_address = decoded_location.contract_address,
+        list_storage_location_nonce = decoded_location.nonce
+    WHERE
+        chain_id = p_chain_id
+        AND contract_address = normalized_contract_address
+        AND token_id = p_token_id;
+END;
+$$;
+
+
+
+-------------------------------------------------------------------------------
 -- Function: handle_contract_event__NewAccountMetadataValue
 -- Description: Inserts or updates an account metadata value. If a record with
 --              the same chain_id, contract_address, address, and key exists,
@@ -201,9 +252,10 @@ BEGIN
 
     -- Normalize the input addresses to lowercase
     normalized_contract_address := public.normalize_eth_address(p_contract_address);
+    normalized_from_address := public.normalize_eth_address(p_from_address);
     normalized_to_address := public.normalize_eth_address(p_to_address);
 
-    IF p_from_address = '0x0000000000000000000000000000000000000000' THEN
+    IF normalized_from_address = '0x0000000000000000000000000000000000000000' THEN
         -- Attempt to insert new row
         IF EXISTS (
             SELECT 1 FROM public.list_nfts nft
--- src/pubsub/handlers/list-storage-location-change.ts
+++ src/pubsub/handlers/list-storage-location-change.ts
@@ -1,53 +1,34 @@
 import { database } from '#/database'
 import { logger } from '#/logger'
+import { sql, type QueryResult, type RawBuilder } from 'kysely'
 import type { Event } from '../event'
 
 export class ListStorageLocationChangeHandler {
   async onListStorageLocationChange(event: Event): Promise<void> {
     const tokenId: bigint = event.eventParameters.args['tokenId']
     const listStorageLocation: `0x${string}` = event.eventParameters.args['listStorageLocation']
-    const listStorageLocationBytes: Uint8Array = Buffer.from(listStorageLocation.slice(2), 'hex')
 
-    // UPDATE list_nfts
-    // SET list_storage_location = $1,
-    //     list_storage_location_chain_id = $2,
-    //     list_storage_location_contract_address = $3,
-    //     list_storage_location_nonce = $4
-    // WHERE chain_id = $2
-    //       AND contract_address = $3
-    //       AND token_id = $4
-    let query = database.updateTable('list_nfts').set({ list_storage_location: listStorageLocation })
-    if (listStorageLocationBytes.length === 1 + 1 + 32 + 20 + 32) {
-      // [0] version byte should be 0x01
-      // [1] list storage location type byte should be 0x01
-      if (listStorageLocationBytes[0] === 0x01 && listStorageLocationBytes[1] === 0x01) {
-        // [2-33] chain id
-        const chainIdBytes: Uint8Array = listStorageLocationBytes.slice(2, 2 + 32)
-        const chainId: bigint = chainIdBytes.reduce((acc, cur) => acc * 256n + BigInt(cur), 0n)
-        // [34-53] contract address
-        const contractAddressBytes: Uint8Array = listStorageLocationBytes.slice(2 + 32, 2 + 32 + 20)
-        const contractAddress: `0x${string}` = `0x${Buffer.from(contractAddressBytes).toString('hex')}`
-        // [54-85] nonce
-        const nonceBytes: Uint8Array = listStorageLocationBytes.slice(2 + 32 + 20, 2 + 32 + 20 + 32)
-        const nonce: bigint = nonceBytes.reduce((acc, cur) => acc * 256n + BigInt(cur), 0n)
-        query = query.set({
-          list_storage_location_chain_id: chainId,
-          list_storage_location_contract_address: contractAddress,
-          list_storage_location_nonce: nonce
-        })
+    const query: RawBuilder<unknown> = sql`SELECT public.handle_contract_event__ListStorageLocationChange(
+      ${event.chainId},
+      ${event.contractAddress},
+      ${tokenId},
+      ${listStorageLocation}
+    )`
+    const eventSignature: string = `${event.eventParameters.eventName}(${tokenId}, ${listStorageLocation})`
+    logger.info(eventSignature)
+
+    try {
+      const result: QueryResult<unknown> = await query.execute(database)
+      // sleep for 1 sec
+      await new Promise(resolve => setTimeout(resolve, 1000))
+
+      if (!result || result.rows.length === 0) {
+        logger.warn(`${eventSignature} query returned no rows`)
+        return
       }
-    } else {
-      // log error in red
-      logger.log(
-        `\x1b[91m(ListStorageLocationChange) List storage location ${listStorageLocation} is ${
-          listStorageLocationBytes.length
-        } bytes instead of ${1 + 1 + 32 + 20 + 32} bytes\x1b[0m`
-      )
+    } catch (error: any) {
+      logger.error(`${eventSignature} Error processing event: ${error.message}`)
+      process.exit(1)
     }
-    await query
-      .where('chain_id', '=', event.chainId.toString())
-      .where('contract_address', '=', event.contractAddress.toLowerCase())
-      .where('token_id', '=', tokenId.toString())
-      .executeTakeFirst()
   }
 }
