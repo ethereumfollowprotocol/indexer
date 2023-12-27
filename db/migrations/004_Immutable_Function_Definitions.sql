3883a82f5e926553b9880095a29b1fbb67daa3f1
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-12-27 04:12:39 -0800

    cleanup, commenting

--- db/migrations/004_Immutable_Function_Definitions.sql
+++ db/migrations/004_Immutable_Function_Definitions.sql
@@ -104,7 +104,7 @@ $$;
 -- Function: is_list_location_hexstring
 -- Description: Validates that the given string is a valid list location
 --              hexadecimal string. The string must start with '0x' and contain
---              168 hexadecimal characters. The function uses a regular
+--              172 hexadecimal characters. The function uses a regular
 --              expression to validate the format.
 -- Parameters:
 --   - hexstring (TEXT): The hexadecimal string to be validated.
@@ -210,21 +210,40 @@ BEGIN
     -- Convert the hex string (excluding '0x') to bytea
     hex_data := DECODE(SUBSTRING(list_storage_location FROM 3), 'hex');
 
-    -- Extract version and locationType
+    ----------------------------------------
+    -- version
+    ----------------------------------------
     version := GET_BYTE(hex_data, 0);
-    location_type := GET_BYTE(hex_data, 1);
-
-    -- Validate version and locationType
-    IF version != 1 OR location_type != 1 THEN
-        RAISE EXCEPTION 'Invalid version or location type';
+    IF version != 1 THEN
+        RAISE EXCEPTION 'Invalid version: % (expected 1)', version;
     END IF;
+
+    ----------------------------------------
+    -- location_type
+    ----------------------------------------
+    location_type := GET_BYTE(hex_data, 1);
+    IF location_type != 1 THEN
+        RAISE EXCEPTION 'Invalid location type: % (expected 1)', location_type;
+    END IF;
+
+    ----------------------------------------
+    -- chain_id
+    ----------------------------------------
 
     -- Extract chainId (32 bytes) as hex string and convert to bigint
     hex_chain_id := '0x' || ENCODE(SUBSTRING(hex_data FROM 3 FOR 32), 'hex');
     chain_id := public.convert_hex_to_bigint(hex_chain_id);
 
+    ----------------------------------------
+    -- contract_address
+    ----------------------------------------
+
     -- Extract contractAddress (20 bytes to TEXT)
     contract_address := ('0x' || ENCODE(SUBSTRING(hex_data FROM 35 FOR 20), 'hex'))::types.eth_address;
+
+    ----------------------------------------
+    -- nonce
+    ----------------------------------------
 
     -- Extract nonce (32 bytes to TEXT)
     temp_nonce := SUBSTRING(hex_data FROM 55 FOR 32);
