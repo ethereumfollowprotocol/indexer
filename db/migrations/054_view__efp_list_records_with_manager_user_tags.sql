b258fa53bae09eecb3d10195df237e4fc86fb395
Cory Gabrielsen <corygabrielsen@gmail.com>
2023-12-30 04:42:31 -0800

    Add view__efp_list_records_with_manager_user_tags

---
+++ db/migrations/054_view__efp_list_records_with_manager_user_tags.sql
@@ -0,0 +1,37 @@
+-- migrate:up
+-------------------------------------------------------------------------------
+-- View: view__efp_list_records_with_nft_manager_user_tags
+-------------------------------------------------------------------------------
+CREATE VIEW PUBLIC.view__efp_list_records_with_nft_manager_user_tags AS
+SELECT
+  nfts.efp_list_nft_chain_id,
+  nfts.efp_list_nft_contract_address,
+  nfts.efp_list_nft_token_id,
+  nfts.efp_list_nft_owner,
+  nfts.efp_list_manager,
+  nfts.efp_list_user,
+  nfts.efp_list_storage_location_chain_id,
+  nfts.efp_list_storage_location_contract_address,
+  nfts.efp_list_storage_location_nonce,
+  record_tags.record,
+  record_tags.record_version,
+  record_tags.record_type,
+  record_tags.record_data,
+  record_tags.tags,
+  CASE
+    WHEN 'block' = ANY (record_tags.tags) THEN TRUE
+    ELSE FALSE
+  END AS has_block_tag,
+  CASE
+    WHEN 'mute' = ANY (record_tags.tags) THEN TRUE
+    ELSE FALSE
+  END AS has_mute_tag
+FROM
+  PUBLIC.view__efp_list_records_with_tags AS record_tags
+  LEFT JOIN PUBLIC.view__efp_list_nfts_with_manager_user AS nfts ON nfts.efp_list_storage_location_chain_id = record_tags.chain_id
+  AND nfts.efp_list_storage_location_contract_address = record_tags.contract_address
+  AND nfts.efp_list_storage_location_nonce = record_tags.nonce;
+
+
+
+-- migrate:down
