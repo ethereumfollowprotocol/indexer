92569221c07dedb85a1265fa52df53d17df42eb1
Cory Gabrielsen <corygabrielsen@gmail.com>
2024-01-23 00:19:41 -0800

    Add query.get_list_records__record_type_001

--- db/queries/api/v1/users/list-records/create_function__get_list_records__record_type_001.sql
+++ db/queries/api/v1/users/list-records/create_function__get_list_records__record_type_001.sql
@@ -1,27 +1,21 @@
 --migrate:up
 -------------------------------------------------------------------------------
--- Function: get_following__record_type_001
--- Description: Retrieves a list of addresses followed by a user from the
---              view_list_records_with_nft_manager_user_tags, ensuring
---              addresses are valid 20-byte, lower-case hexadecimals (0x
---              followed by 40 hex chars). Filters tokens by version and type,
---              excluding blocked or muted relationships. Leverages primary
---              list token ID from get_primary_list. If no primary list is
---              found, returns an empty result set.
+-- Function: get_list_records__record_type_001
+-- Description: Retrieves all list records of a given type for a given user.
 -- Parameters:
---   - address (VARCHAR(42)): Identifier of the user to find the
---                            following addresses.
+--   - p_address (VARCHAR(42)): Identifier of the user to find the list records
+--                              of a given type for the user's primary list.
 -- Returns: A table with 'efp_list_nft_token_id' (BIGINT), 'record_version'
 --          (types.uint8), 'record_type' (types.uint8), and 'following_address'
 --          (types.eth_address), representing the list token ID, record
 --          version, record type, and following address.
 -------------------------------------------------------------------------------
 CREATE
-OR REPLACE FUNCTION query.get_following__record_type_001 (p_address VARCHAR(42)) RETURNS TABLE (
+OR REPLACE FUNCTION query.get_list_records__record_type_001 (p_address VARCHAR(42)) RETURNS TABLE (
   efp_list_nft_token_id BIGINT,
   record_version types.uint8,
   record_type types.uint8,
-  following_address types.eth_address,
+  address types.eth_address,
   tags types.efp_tag []
 ) LANGUAGE plpgsql AS $$
 DECLARE
@@ -59,13 +53,13 @@ BEGIN
     WHERE
       v.efp_list_nft_token_id = primary_list_token_id;
 
-    -- following query
+    -- list records query
     RETURN QUERY
     SELECT
         (primary_list_token_id)::BIGINT AS efp_list_nft_token_id,
         v.record_version,
         v.record_type,
-        PUBLIC.hexlify(v.record_data)::types.eth_address AS following_address,
+        PUBLIC.hexlify(v.record_data)::types.eth_address AS address,
         v.tags
     FROM
         public.view__join__efp_list_records_with_tags AS v
@@ -78,12 +72,7 @@ BEGIN
         -- address record type (1)
         v.record_type = 1 AND
         -- where the address record data field is a valid address
-        public.is_valid_address(v.record_data) AND
-        -- NOT blocked or muted
-        NOT EXISTS (
-            SELECT 1 FROM unnest(v.tags) as tag
-            WHERE tag IN ('block', 'mute')
-        )
+        public.is_valid_address(v.record_data)
     ORDER BY
         v.record_version ASC,
         v.record_type ASC,
