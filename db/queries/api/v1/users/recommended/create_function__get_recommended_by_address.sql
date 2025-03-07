f797d4f998b955924d0d6b19cd2e251f138fd5bd
0xthrpw <0xthrpw@gmail.com>
2025-03-07 11:45:12 -0500

    recommended, discover

--- db/queries/api/v1/users/recommended/create_function__get_recommended_by_address.sql
+++ db/queries/api/v1/users/recommended/create_function__get_recommended_by_address.sql
@@ -7,6 +7,7 @@ OR REPLACE FUNCTION query.get_recommended_by_address (p_address types.eth_addres
   name TEXT,
   address types.eth_address,
   avatar TEXT,
+  header TEXT,
   class TEXT,
   created_at TIMESTAMP WITH TIME ZONE
 ) LANGUAGE plpgsql AS $$
@@ -19,14 +20,17 @@ BEGIN
         efp_recommended.name,
         efp_recommended.address,
         efp_recommended.avatar,
+        meta.records->>'header' as header,
         efp_recommended.class,
         efp_recommended.created_at
     FROM public.efp_recommended
+    LEFT JOIN public.ens_metadata meta ON efp_recommended.address = meta.address
     WHERE NOT EXISTS (
         SELECT 1 
         FROM query.get_all_following__record_type_001(normalized_addr) fol
         WHERE efp_recommended.address = fol.following_address
     )
+    AND efp_recommended.address <> normalized_addr
     ORDER BY efp_recommended.index
     LIMIT p_limit   
     OFFSET p_offset;
